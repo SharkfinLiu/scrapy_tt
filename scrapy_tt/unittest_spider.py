c925476a3b85d37f53de3c032151f7e5f58a564c
Unknown <419694859@qq.com>
2017-11-24 17:20:13 +0800

    table

--- scrapy_tt/unittest_spider.py
+++ scrapy_tt/unittest_spider.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 import unittest
+import re
 from scrapy.crawler import CrawlerProcess
 from scrapy_tt.spiders.yule_star import *
 from scrapy_tt.spiders.dc3 import *
 from scrapy_tt.spiders.dc3_for import *
 from scrapy_tt.spiders.dc3_digui import *
+from scrapy_tt.spiders.dc3_for_2 import *
 
 class SpidersTestCase(unittest.TestCase):
 
@@ -80,8 +82,8 @@ class SpidersTestCase(unittest.TestCase):
     def test_dc3_for(self):
             # scr_list = '然后从前向后，每三个字符一组，即aba,aaa,ba0,baa,aab,a00，我们发现，他们分别是Suffix[1],Suffix[4],Suffix[7],Suffix[2],Suffix[5],Suffix[8] 的前3个字母。我们求出这六个的排名为4,2,5,6,3,1（注意，如果排序后还有相同的数字，也就是还有两个相同的串，比如3,2,4,5,2,1，那么要继续求，因为两个2之后的数字4大于1，所以第二的位置的2代表的后缀大于第5个位置的2代表的后缀。其实这个问题跟刚才的问题是相同的，所以可以递归求）。这样，我们最后得到了所有模3不等于0的位置的后缀的排名。|'
             # src_list = list(scr_list)
-        # src_list = ['侠客岛', '特朗普', '访华', '前', '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前', '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前', '美国', '送', '了', '中国', '一份', '不大不小', '的', '礼物', '|']
-        src_list = ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
+        src_list = ['侠客岛', '特朗普', '访华', '前', '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前', '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前', '美国', '送', '了', '中国', '一份', '不大不小', '的', '礼物', '|']
+        # src_list = ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
         # src_list = ['1']
         s = []
         result_list = dc3_for_input(src_list)
@@ -95,10 +97,13 @@ class SpidersTestCase(unittest.TestCase):
 
     def test_sort_chinese(self):
         # src_list = ['|','利','反','媒','激','特','税','英','计','遭']
-        src_list = ['使用','cookie','登陆','比较','简单','，','但是','有','时效性','问题','。','验证码','识别','是','个','很好','的','思路','，','然而','识别','的','精度','又','限制','了','抓取','的','效率','。','使用','cookie','登陆','比较','简单','，']
+        src_list = ['侠客岛', '特朗普', '访华', '前', '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前',
+                    '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前', '美国', '送', '了', '中国',
+                    '一份', '不大不小', '的', '礼物', '|']
         src_list = list(set(src_list))
         src_list.sort()
-        print(src_list)
+        for i,val in enumerate(src_list):
+            print(str(i)+':'+val)
 
     def test_q(self):
         count = 2
@@ -130,3 +135,61 @@ class SpidersTestCase(unittest.TestCase):
             print('sa[' + str(i) + ']')
             print(s)
             s = []
+
+    def test_dc3_for_2(self):
+
+        # scr_list = '然后从前向后，每三个字符一组，即aba,aaa,ba0,baa,aab,a00，我们发现，他们分别是Suffix[1],Suffix[4],Suffix[7],Suffix[2],Suffix[5],Suffix[8] 的前3个字母。我们求出这六个的排名为4,2,5,6,3,1（注意，如果排序后还有相同的数字，也就是还有两个相同的串，比如3,2,4,5,2,1，那么要继续求，因为两个2之后的数字4大于1，所以第二的位置的2代表的后缀大于第5个位置的2代表的后缀。其实这个问题跟刚才的问题是相同的，所以可以递归求）。这样，我们最后得到了所有模3不等于0的位置的后缀的排名。|'
+        # src_list = list(scr_list)
+        src_list = ['侠客岛', '特朗普', '访华', '前', '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前',
+                    '美国', '给', '中国', '送', '了', '份', '不大不小', '的', '礼物', '|', '特朗普', '访华', '前', '美国', '送', '了', '中国',
+                    '一份', '不大不小', '的', '礼物', '|']
+        # src_list = ['11','17','16','15','19','07','04','12','02','11','09','16','05','19','08','04','14','02','17','10','18','06','03','04','13','01']
+        # src_list = ['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
+        # src_list = ['1']
+        s = []
+        result_list = dc3_input_for_2(src_list)
+        print(result_list)
+        for i in result_list:
+            for j in range(i, len(src_list)):
+                s.append(src_list[j])
+            print('sa[' + str(i) + ']')
+            print(s)
+            s = []
+
+    def test_selector(self):
+        selector_css = 'body > table:nth-child(3) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(4) >table:nth-child(0) > tr:nth-child(2) > .px12c > table > tr > td > div > founder-content *::text'
+        css_re = re.compile(r'table[\-:()0-9a-zA-Z]*\s>')
+        add_tbody = ' tbody >'
+        t = css_re.finditer(selector_css)
+        count = 0
+        # for i in t:
+        #     count += 1
+        # print(count)
+        star =[0]
+        end =[]
+        for i in t:
+            star.append(i.span()[0])
+            end.append(i.span()[-1])
+            # add_tbody = i.group() + ' tbody >'
+        print(star)
+        print(end)
+        list_len = len(star)
+        table = []
+        other = []
+        for i in list_len:
+            other.append(selector_css[:star[i]])
+            table.append(selector_css[star[i]:end[i]])
+        # print(count)
+        # star.append(selector_css[:i.span()[0]])
+        # end.append(selector_css[i.span()[-1]:])
+
+    def test_plzh(self):
+        import itertools
+        list1 = [1, 2, 3]
+        list2 = []
+        for i in range(1, len(list1) + 1):
+            iter = itertools.combinations(list1, i)
+            list2.append(list(iter))
+        for lis in list2:
+            for li in lis:
+                print(list(li))
